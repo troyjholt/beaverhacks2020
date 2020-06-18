8feccb088215978cdf273de18e1dc0c7d8c2e6c9
Aaron Thompson <58874671+lastoryx@users.noreply.github.com>
2020-06-18 11:51:05 -0700

    Initial skeleton structure.

---
+++ script.js
@@ -0,0 +1,53 @@
+/* Include and start express. */
+let express = require('express');
+let app = express();
+
+/* Path module for directing to the Public assets folder.*/
+const path = require('path');
+
+/* Set the path for loading assets like CSS and images.*/
+app.use(express.static(path.join(__dirname, '/public')));
+
+/* Start express-handlebars. Set the main layout. */
+let handlebars = require('express-handlebars').create({defaultLayout: 'index'});
+
+/* Set express-handlebars as the template engine.*/
+
+app.engine('handlebars', handlebars.engine);  // Connect express-handlebars to express.
+app.set('view engine', 'handlebars'); // Set express-handlebars to manage the views.
+app.set('port', process.argv[2]); // Set input from the console to manage the port.
+
+/* Set up the HTTP request.*/
+let request = require('request');
+
+/*This code sets up the middleware used to parse POST requests
+ * sent via the body. It can accept posts that are URL-Encoded
+ * or JSON formatted.*/
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+/* THIS SETUP DOES NOT INCLUDE SESSIONS SETUP OR MYSQL SETUP */
+
+
+/* This GET request handles loading the front page. */
+app.get('/', function (req, res) {
+    let context = {};
+    res.render('front', context);
+});
+
+/* Create a 500 page. */
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+/* Create a 404 page. */
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
